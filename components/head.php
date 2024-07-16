4bc41b4b32d36da624912a3ba2b32fb92174bd90
Tejas Vaishnav <tejas.vaishnav@gmail.com>
2024-07-16 16:52:08 +0530

    add php files

---
+++ components/head.php
@@ -0,0 +1,20 @@
+<?php
+            define('base_url', 'http://kcgclients.com/shaby/site2/');
+            $pageName = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    link
+</head>
+<body>
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require_once("components/head.php");  ?>
+<body>
+    <h1>This is php home!!</h1>
+</body>
+</html>
